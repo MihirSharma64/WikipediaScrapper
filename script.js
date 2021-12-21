2b21f6c1141ffbd35b9815ef218942f1b4c19ab4
Mihir Sharma <mihirsharma64@gmail.com>
2021-12-21 18:50:53 +0530

    Completed text scrap part

--- script.js
+++ script.js
@@ -24,7 +24,46 @@ async function main(){
 	await azIndexBtn[0].click();
     ///////////////////////////////////////
 
+    /*------------------- Clicking M ---------------*/
+    await browser.wait(wd.until.elementLocated(wd.By.css('a[title="Special:AllPages/M"]')));
+
+    let mLetterBtn = await browser.findElements(wd.By.css('a[title="Special:AllPages/M"]'));
+	await mLetterBtn[0].click();
+    /*----------------------------------------------*/
+
+    /*--------------------Clicking M article---------*/
+    await browser.wait(wd.until.elementLocated(wd.By.css('.mw-allpages-chunk')));
+
+    let mArtcile = await browser.findElements(wd.By.css('.mw-allpages-chunk li a'));
+	await mArtcile[0].click();
+
+    /*----------------------------------------------- */
+
+    /*--------------- Content -------------------*/
+
+    await browser.wait(wd.until.elementLocated(wd.By.css('table.wikitable + p')));
+
+    let paragraphs = await browser.findElements(wd.By.css('table.wikitable ~ p'));
+	let historyParaText = await paragraphs[0].getAttribute('innerText') ;
+    console.log(historyParaText);
+
+    let usesPara1 = await paragraphs[1].getAttribute('innerText');
+    usesPara1 += await paragraphs[2].getAttribute('innerText');
+    console.log(usesPara1);
+
+    let OtherUsesList = await browser.findElements(wd.By.css('table.wikitable ~ ul'));
+    let listItems = await OtherUsesList[0].findElements(wd.By.css('li'));
     
+    // let text = await listItems[0].getAttribute('innerText');
+    // console.log(text);/
+    let OtherUsesText='';
+    for(let i=0;i<listItems.length;i++){
+        let text = await listItems[i].getAttribute('innerText');
+        OtherUsesText = OtherUsesText + "\n" + text;
+    }
+    console.log(OtherUsesText);
+
+    /*--------------------------------------------- */
 
 
 
